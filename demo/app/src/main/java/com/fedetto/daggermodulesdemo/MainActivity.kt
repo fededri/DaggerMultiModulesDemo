eee79b6058fe0899f8316076bc4daf622d9082b6
Federico Torres <federico.torres@softvision.com>
2020-06-25 21:39:17 -0300

    return to login

--- demo/app/src/main/java/com/fedetto/daggermodulesdemo/MainActivity.kt
+++ demo/app/src/main/java/com/fedetto/daggermodulesdemo/MainActivity.kt
@@ -2,10 +2,33 @@ package com.fedetto.daggermodulesdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.fedetto.core.IntentProvider
+import com.fedetto.core.UserController
+import dagger.android.AndroidInjection
+import dagger.android.support.AndroidSupportInjection
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var userController: UserController
+
+    @Inject
+    lateinit var intentProvider: IntentProvider
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        findViewById<TextView>(R.id.textView).text = "Hello ${userController.getName()}"
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            startActivity(intentProvider.loginActivity())
+        }
+
     }
 }
